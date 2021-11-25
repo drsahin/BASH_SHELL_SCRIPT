107abfc06a1ed6685a6534fb1bd25bb65b1c142c
drsahin <drsahin5555@gmail.com>
2021-11-25 22:08:09 +0300

    yeni soru eklendi.

--- python1.py
+++ python1.py
@@ -9,3 +9,14 @@ if total == num :
   print("{} is a perfect number.".format(num))
 else:
   print("{} is not a perfect number.".format(num))
+
+
+  n = int(input("Enter a positive number:"))
+count = 0
+for i in range(2,n) :
+  if n % i == 0 :
+    count += 1
+if n==0 or n == 1 or count > 0 :
+  print(n, "is not a prime number")
+else :
+  print(n, "is a prime number")
